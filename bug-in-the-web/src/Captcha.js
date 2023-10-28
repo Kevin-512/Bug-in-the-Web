b9f9212f0120fe7f31d5f2fcd2430e0241b02c37
HualiangZ <zhualiang88@gmail.com>
2023-10-28 15:16:58 +0100

    init image folder

---
+++ bug-in-the-web/src/Captcha.js
@@ -0,0 +1,10 @@
+import React from "react";
+import captcha from'./images/captcha.png';
+
+function App() {
+  return (
+    <img src={captcha} alt="fireSpot"/>
+  );
+}
+
+export default App;
--- bug-in-the-web/src/index.js
+++ bug-in-the-web/src/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import Captcha from './Captcha';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Captcha />
   </React.StrictMode>
 );
 
